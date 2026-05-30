1091be776bb60a4465aef30a2f569ab817a13eec
DoomFishy <calvinlu350@gmail.com>
2026-05-29 21:37:39 -0400

    implemented encoding header

--- encode.js
+++ encode.js
@@ -6,63 +6,78 @@ export class StegoEncoder {
         this.cover_data;
         this.secret_data;
 
-        this.cover_width;
-        this.cover_height
-        this.secret_width;
-        this.secret_height
-
         this.password;
-        this.lsb_bits; 
+        this.lsb_bits = 7; 
 
         this.header_positions;
         this.data_positions;
     }
 
-    encode(image_data, password){
-        this.setPassword(password);
-
-        let generator = new GeneratePositions(this.cover_width, this.cover_height, this.password);
+    encode(password){
+        let generator = new GeneratePositions(this.cover_data.width, this.cover_data.height, password);
         this.positions = generator.getScrambledPositions();
 
         this.header_positions = this.positions.slice(0, 64);
         this.data_positions = this.positions.slice(64, this.positions.length);
 
-        let x = this.positions[0].x;
-        let y = this.positions[0].y;
 
-        let image = this.hideHeader(this.header_positions);
-        let image = this.hideData(this.data_positions);
+        let image = this.hideHeader(this.cover_data, this.cover_data.width, this.header_positions);
+        image = this.hideHeader(this.cover_data, this.cover_data.height, this.header_positions);
 
+        image = this.hideData(this.cover_data, this.secret_data, this.data_positions);
+        return image;
     }
 
-    hideHeader(header_positions){
-        let size = header_positions.length;
+    hideHeader(target, dimension, header_positions){
+        let size = header_positions.length / 2;
 
         for (let i = 0; i < size; i++){
-            pixel_channel = getPixel(header_positions[i].x, header_positions[i].y, this.cover_data, this.width);
+            let x = header_positions[i].x
+            let y = header_positions[i].y
+
+            let target_pixel = this.getPixel(x, y, target.data, target.width);
+
+            let shift = (size - 1 - i) * this.lsb_bits * 3;
+            
+            let r = (dimension >> (shift + this.lsb_bits * 2)) & ((1 << this.lsb_bits) - 1);
+            let g = (dimension >> (shift + this.lsb_bits)) & ((1 << this.lsb_bits) - 1);
+            let b = (dimension >> shift) & ((1 << this.lsb_bits) - 1);
+
+            let new_r = this.hideBits(target_pixel.r, r, this.lsb_bits);
+            let new_g = this.hideBits(target_pixel.g, g, this.lsb_bits);
+            let new_b = this.hideBits(target_pixel.b, b, this.lsb_bits);
+
+            let new_pixel = {r: new_r, g: new_g, b: new_b};
+
+            target.data = this.setPixel(x, y, target.data, new_pixel, target.width);
         }
     }
 
     hideData(target, source, data_positions){
-        let image_data;
+        let index = 0;
 
         for (let i = 0; i < target.height; i++){
             for (let j = 0; j < target.width; j++){
                 
                 if (i < source.height){
                     if (j < source.width){
-                        let x = data_positions[j].x
-                        let y = data_positions[j].y
+                        let x = data_positions[index].x
+                        let y = data_positions[index].y
 
-                        target_pixel = this.getPixel(x, y, target.data, target.width)
-                        source_pixel = this.getPixel(x, y, source.data, source.width)
+                        let target_pixel = this.getPixel(x, y, target.data, target.width);
+                        let source_pixel = this.getPixel(j, i, source.data, source.width);
 
-                        target_pixel.r = hidBits(target_pixel.r, source_pixel.r, this.lsb);
-                        target_pixel.g = hidBits(target_pixel.r, source_pixel.r, this.lsb);
-                        target_pixel.b = hidBits(target_pixel.r, source_pixel.r, this.lsb);
+                        let r = this.hideBits(target_pixel.r, source_pixel.r, this.lsb_bits);
+                        let g = this.hideBits(target_pixel.g, source_pixel.g, this.lsb_bits);
+                        let b = this.hideBits(target_pixel.b, source_pixel.b, this.lsb_bits);
 
-                        image = setPixel(x, y, target_pixel)
+                        let new_pixel = {r: r, g: g, b: b};
+                        target.data = this.setPixel(x, y, target.data, new_pixel, target.width);
+                        index++;
                     }
+                }
+                else {
+                    return target;
                 }
             }
         }
@@ -71,19 +86,24 @@ export class StegoEncoder {
     }
 
     hideBits(target, source, lsb){
+
         for (let i = 0; i < lsb; i++){
-            bit = (source >> i) & 1;
+            let bit = (source >> i) & 1;
 
             if (bit == 1){
+                //console.log("Was " + target.toString(2) + " replaced with 1");
                 target = target | (1 << i);
+                //console.log("Now " + target.toString(2));
             }
 
             else{
+                //console.log("Was " + target.toString(2) + " replaced with 0");
                 target = target & ~(1 << i);
+                //console.log("Now " + target.toString(2));
             }
         }
 
-        return target
+        return target;
     }
 
     getPixel(x, y, image_data, width){
@@ -96,26 +116,23 @@ export class StegoEncoder {
         }
     }
 
-    setPixel(x, y, target_data, source_data, width){
+    setPixel(x, y, target_data, pixel, width){
         const index = (y * width + x) * 4;
 
-        target_data[index].r = source_data[index].r
-        target_data[index + 1].g = source_data[index + 1].g
-        target_data[index + 2].b = source_data[index + 2].b
+        target_data[index] = pixel.r;
+        target_data[index + 1] = pixel.g;
+        target_data[index + 2] = pixel.b;
 
-        return target_data
+        return target_data;
     }
 
     setCoverImage(image_data){
-        this.cover_data = image_data.data;
-        this.cover_width = image_data.width;
-        this.cover_height = image_data.height;
+        this.cover_data = image_data;
     }
 
     setSecretImage(image_data){
-        this.secret_data = image_data.data;
-        this.secret_width = image_data.width;
-        this.secret_height = image_data.height;    
+        this.secret_data = image_data;
+ 
     }
 
     setPassword(password){
--- main.js
+++ main.js
@@ -32,17 +32,67 @@ document.getElementById('upload-cover').addEventListener('change', function(e) {
         width: canvas.width,
         height: canvas.height
       });
-
-      encoder.encode({
-        data: pixels,
-        width: canvas.width,
-        height: canvas.height
-      }, "1234");
     }
-
 
     img.src = URL.createObjectURL(file);
 
   };
 });
+
+
+document.getElementById('upload-secret').addEventListener('change', function(e) {
+  const file = e.target.files[0];
+    
+  if (file && file.type.startsWith('image/')) {
+    const img = new Image();
+
+    img.onload = function(){
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const pixels = imageData.data; // extract pixels
+
+      /*
+      for (let i = 0; i < pixels.length; i+= 4){
+        pixels[i] = (pixels[i] & 0xFE) | 1; // Red
+        pixels[i+1] = (pixels[i+1] & 0xFE) | 1; // Green
+        pixels[i+2] = (pixels[i+2] & 0xFE) | 1; // Blue
+      }
+      */
+
+      encoder.setSecretImage({
+        data: pixels,
+        width: canvas.width,
+        height: canvas.height
+      });
+    }
+
+    img.src = URL.createObjectURL(file);
+
+  };
+});
+
+document.getElementById("submit").onclick = () =>{
+  const image = encoder.encode("1234");
+
+  const canvas = document.getElementById("myCanvas");
+  canvas.width = image.width;
+  canvas.height = image.height;
+  
+  // Create ImageData object
+  const imageData = new ImageData(image.data, image.width, image.height);
+  
+  // Draw to canvas
+  const ctx = canvas.getContext("2d");
+  ctx.putImageData(imageData, 0, 0);
+
+}
+
+
+
 
